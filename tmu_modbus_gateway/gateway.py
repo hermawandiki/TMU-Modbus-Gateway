7a19c5d343c7992f743586faf0be68fa98169077
Diki Hermawan <lalikatasandi1403@gmail.com>
2026-06-26 08:47:20 +0700

    add config.json and log

--- tmu_modbus_gateway/gateway.py
+++ tmu_modbus_gateway/gateway.py
@@ -3,14 +3,41 @@ import struct
 import threading
 import serial
 import time
+import json
+import sys
+import os
+import logging
 
-HOST = "0.0.0.0"
-PORT_SLAVE_MAP = {502: 1, 503: 2, 504: 3}
-SERIAL_CFG = {
-    "port": "COM10", 
-    "baudrate": 9600, 
-    "timeout": 1.0,
-}
+ts = time.strftime("%Y%m%d")
+logName = r'D:/GitHub/TMU-Modbus-Gateway/tmu_modbus_gateway/logsys/logsys-' + ts + '.log'
+logging.basicConfig(
+    filename=logName,
+    format='%(asctime)s | %(levelname)s: %(message)s',
+    level=logging.DEBUG
+)
+
+def load_config(filename="config.json"):
+    try:
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        config_path = os.path.join(base_dir, filename)
+        logging.info(f"Loading config from: {config_path}")
+        
+        with open(config_path, "r") as f:
+            config = json.load(f)
+            
+        host = config.get("host", "0.0.0.0")
+        serial_cfg = config.get("serial_port", {})
+        
+        port_slave_map = {int(k): v for k, v in config.get("port_slave_map", {}).items()}
+        
+        logging.info(f"Config loaded with OK")
+        return host, serial_cfg, port_slave_map
+    except FileNotFoundError:
+        logging.error(f"File {filename} not found.")
+        sys.exit(1)
+    except json.JSONDecodeError:
+        logging.error(f"Format {filename} invalid.")
+        sys.exit(1)
 
 def calculate_crc16(data: bytes) -> bytes:
     crc = 0xFFFF
@@ -32,13 +59,14 @@ def rtu2tcp(rtu_frame: bytes, tx_id: bytes, unit_id: int) -> bytes:
     return tx_id + b"\x00\x00" + struct.pack(">H", len(pdu) + 1) + bytes([unit_id]) + pdu
 
 class SerialBus:
-    def __init__(self):
+    def __init__(self, serial_cfg: dict):
         self._lock = threading.Lock()
+        self.timeout = serial_cfg.get("timeout", 1.0)
         try:
-            self._ser = serial.Serial(**SERIAL_CFG)
-            print(f"Connected to {SERIAL_CFG['port']} @ {SERIAL_CFG['baudrate']}")
+            self._ser = serial.Serial(**serial_cfg)
+            logging.info(f"Connected to {serial_cfg.get('port')} at {serial_cfg.get('baudrate')}")
         except Exception as e:
-            print(f"Failed to open serial: {e}")
+            logging.error(f"Failed to open serial: {e}")
             self._ser = None
 
     def send_and_receive(self, request: bytes) -> bytes:
@@ -52,7 +80,7 @@ class SerialBus:
                 response = b""
                 start = time.time()
                 
-                while time.time() - start < SERIAL_CFG["timeout"]:
+                while time.time() - start < self.timeout:
                     if self._ser.in_waiting:
                         response += self._ser.read(self._ser.in_waiting)
                         if len(response) >= 5 and validate_crc16(response):
@@ -64,7 +92,7 @@ class SerialBus:
                 return b""
 
 def handle_client(conn: socket.socket, addr, port: int, slave_id: int, bus: SerialBus):
-    print(f"\nConnected to {port} -> Slave {slave_id} from {addr[0]}:{addr[1]}")
+    logging.info(f"Connected to {port} -> Slave {slave_id} from {addr[0]}:{addr[1]}")
     try:
         while True:
             tcp_req = conn.recv(1024)
@@ -74,28 +102,29 @@ def handle_client(conn: socket.socket, addr, port: int, slave_id: int, bus: Seri
             tx_id, unit_id, fc = tcp_req[0:2], tcp_req[6], tcp_req[7]
 
             rtu_req = tcp2rtu(tcp_req, slave_id)
-            print(f"[REQ]  {rtu_req.hex(' ')}")
+            logging.info(f"[REQ]  {rtu_req.hex(' ')}")
             rtu_res = bus.send_and_receive(rtu_req)
 
             if rtu_res and validate_crc16(rtu_res):
                 conn.sendall(rtu2tcp(rtu_res, tx_id, unit_id))
-                print(f"[OK]  {rtu_res.hex(' ')}")
+                logging.info(f"[OK]  {rtu_res.hex(' ')}")
             else:
                 exc = bytes([fc | 0x80, 0x0B])
                 conn.sendall(tx_id + b"\x00\x00" + struct.pack(">H", len(exc) + 1) + bytes([unit_id]) + exc)
-                print("[ERR] Respond Timeout / CRC Invalid")
-                print(f"[ERR] {rtu_res.hex(' ')}" if rtu_res else "[ERR] No Response")
-            print("")
-                
+                logging.error(f"[ERROR] Respond Timeout / CRC Invalid")
+                logging.error(f"[ERROR] {rtu_res.hex(' ')}" if rtu_res else "[ERROR] No Response")
+            logging.info("")
+    except ConnectionResetError:
+        logging.warning(f"[INFO ] Connection reset by {addr[0]}:{addr[1]}")
     except Exception: pass
     finally:
         conn.close()
-        print(f"Disconnected Port {port} from {addr[0]}:{addr[1]}")
+        logging.info(f"[INFO ] Disconnected Port {port} from {addr[0]}:{addr[1]}")
 
-def start_listener(port: int, slave_id: int, bus: SerialBus):
+def start_listener(host: str, port: int, slave_id: int, bus: SerialBus):
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server.bind((HOST, port))
+    server.bind((host, port))
     server.listen(5)
 
     while True:
@@ -103,17 +132,18 @@ def start_listener(port: int, slave_id: int, bus: SerialBus):
         threading.Thread(target=handle_client, args=(conn, addr, port, slave_id, bus), daemon=True).start()
 
 def main():
-    print("=== TMU Modbus Gateway ===")
-    bus = SerialBus()
+    logging.info("=== TMU Modbus Gateway ===")
+    host, serial_cfg, port_slave_map = load_config("config.json")
+    bus = SerialBus(serial_cfg)
 
-    for port, slave_id in PORT_SLAVE_MAP.items():
-        threading.Thread(target=start_listener, args=(port, slave_id, bus), daemon=True).start()
-        print(f"Listen: {HOST}:{port}  ->  Slave ID {slave_id}")
+    for port, slave_id in port_slave_map.items():
+        threading.Thread(target=start_listener, args=(host, port, slave_id, bus), daemon=True).start()
+        logging.info(f"[INFO ] Ready listening on Port {port} for Slave ID {slave_id}")
 
     try:
         threading.Event().wait()
     except KeyboardInterrupt:
-        print("\nGateway Stopped.")
+        logging.info("\n[INFO ] Gateway Stopped.")
 
 if __name__ == "__main__":
     main()
